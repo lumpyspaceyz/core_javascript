f0eda36d88b3e0cd3289e663eb024a1b496c5173
lumpyspaceyz <yzever99@gmail.com>
2023-11-24 16:36:53 +0900

    [01.core] Code Structure / Variables / Strict Mode / Global This / Legacy var / Data Types

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,43 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+// let admin;
+// let name;
+// name = 'John';
+// admin = name;
+// alert(admin);
+
+// let ourPlanetName = "Earth";
+// let currentUserName = "John";
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+/* variables ----------------------------------------------------------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceByQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasCartList = false;
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 오늘의 운세
+let fotuneOfToday = 'nice';
+
+/* constant variables -------------------------------------------------- */
+
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 상품 정보
+const productInformation = '';
---
+++ client/chapter/core/06.dataTypes.js
@@ -0,0 +1,56 @@
+/* ------------------------ */
+/* Data Types               */
+/* ------------------------ */
+
+/* ECMAScript의 8가지 데이터 타입 -------------------------------------------- */
+
+// 1. 존재하지 않는(nothing) 값 / 비어있는(empty) 값 / 알 수 없는(unknown) 값
+let empty = null;
+console.log(typeof empty); // object
+
+// 2. 값이 할당되지 않은 상태
+let undef;
+console.log(typeof undef); // undefined
+
+// 3. 따옴표를 사용해 묶은 텍스트(큰", 작은', 역`)
+const double = 'hello';
+const single = 'hello';
+const backtick = `hello ${10 + 30}`;
+
+console.log(typeof double); // string
+console.log(typeof single); // string
+console.log(typeof backtick); // string
+
+// 4. 정수, 부동 소수점 숫자(길이 제약)
+const integer = 150;
+const floatingPointNumber = 10.123;
+
+console.log(typeof integer); // number
+console.log(typeof floatingPointNumber); // number
+console.log(typeof NaN); // number
+console.log(typeof Infinity); // number
+console.log(typeof -Infinity); // number
+
+// 5. 길이에 제약이 없는 정수(예: 암호 관련 작업에서 사용)
+const bigNumber = 123n;
+
+console.log(typeof bigNumber); // bigint
+
+// 6. 참(true, yes) 또는 거짓(false, no)
+// 7. 데이터 컬렉션(collection) 또는 복잡한 엔티티(entity)
+// 8. 고유한 식별자(unique identifier)
+
+/* typeof 연산자의 2가지 사용법 ---------------------------------------------- */
+
+// 1) 연산자 typeof
+// 2) 함수 typeof()
+
+// 언어 상, 오류
+
+// Object
+
+// Array
+
+// function
+
+// this
