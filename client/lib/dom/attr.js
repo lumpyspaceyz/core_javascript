d6b17d6df16402d4ec475bd1f2af82424b568a73
lumpyspaceyz <yzever99@gmail.com>
2023-12-12 17:16:59 +0900

    [02.dom] BOM / DOM / Node Class / Attribute / Manipulation [lid] arrt.js / getNode.js

---
+++ client/lib/dom/attr.js
@@ -0,0 +1,49 @@
+function getAttr(node, prop) {
+
+  if(typeof node === 'string') node = getNode(node);
+  if(typeof prop !== 'string') throw new TypeError('getAttr 함수의 두 번째 인수는 문자 타입이어야 합니다.');
+
+  return node?.getAttribute(prop);
+}
+
+
+
+function setAttr(node,prop,value){
+
+  
+  if(typeof node === 'string') node = getNode(node);
+  if(typeof prop !== 'string' || typeof value !== 'string') throw new TypeError('setAttr함수의 두 번째 인수는 문자 타입 이어야 합니다.');
+
+  if(value === ''){
+    node.removeAttribute(prop);
+    return;
+  }
+
+  if(!value) throw new Error('setAttr함수의 세 번째 인수는 필수 입력값 입니다.')
+
+  if(prop.startsWith('data')){
+    prop = prop.slice(5);
+    node.dataset[prop] = value;
+    return;
+  }
+
+  node.setAttribute(prop,value)
+}
+
+
+
+function setAttrYz(node, prop, value) {
+  
+  if(typeof node === 'string') node = getNode(node);
+  if(typeof prop !== 'string') throw new TypeError('setAttr 함수의 두 번째 인수는 문자 타입이어야 합니다.');
+  if(typeof value !== 'string') throw new TypeError('setAttr 함수의 세 번째 인수는 문자 타입이어야 합니다.');
+
+  return node?.setAttribute(prop, value);
+}
+
+
+
+const attr = (node, prop, value) => value === undefined ? getAttr(node, prop) : setAttr(node, prop, value);
+
+attr('.first', 'class');           // getter
+attr('.first', 'class', 'hello');  // setter
